c76375c947b71b4a31de34df193baabfadf10720
Enrique López de Roda López <75045769+e-lrl@users.noreply.github.com>
2024-10-11 15:51:55 +0200

    Actualizar __init__.py

--- server/__init__.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-from .routes import configure_routes
-
-# Crear la instancia de Flask
-app = Flask(__name__)
-
-# Configurar las rutas (importando desde routes.py)
-configure_routes(app)
