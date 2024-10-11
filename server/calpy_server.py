8e6743185571d92698baf4ed6d3e257e6d90f653
Enrique López de Roda López <75045769+e-lrl@users.noreply.github.com>
2024-10-11 16:38:35 +0200

    Actualizar calpy_server.py

---
+++ server/calpy_server.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from auth.routes import auth_bp
+from config import Config
+
+app = Flask(__name__)
+app.config.from_object(Config)
+
+# Registrar el blueprint para autenticación
+app.register_blueprint(auth_bp)
+
+if __name__ == '__main__':
+    app.run(debug=True)
